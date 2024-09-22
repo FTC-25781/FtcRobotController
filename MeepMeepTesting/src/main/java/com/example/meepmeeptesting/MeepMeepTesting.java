2195c44f944d7819c90a0339a8650aa79b0d313c
yuvan-ramesh <helloyuvan@gmail.com>
2024-09-22 15:36:17 -0700

    MeepMeep changes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -24,10 +24,14 @@ public class MeepMeepTesting {
                                 .splineTo(new Vector2d(56, 56), Math.PI / 2)
                                 .splineTo(new Vector2d(60, 24), Math.PI / 2)
                                 .turn(Math.toRadians(90))
-                                .lineTo(new Vector2d(60,24))
-//                                .splineTo(new Vector2d(56, 56), Math.PI / 2)
-//                                .splineTo(new Vector2d(70, 24), Math.PI / 2)
-//                                .splineTo(new Vector2d(56, 56), Math.PI / 2)
+                                .lineTo(new Vector2d(50,24))
+                                .splineTo(new Vector2d(56, 56), Math.PI / 2)
+                                .splineTo(new Vector2d(60, 24), Math.PI / 2)
+                                .turn(Math.toRadians(-90))
+                                .turn(Math.toRadians(180))
+                                .lineTo(new Vector2d(50,24))
+                                .splineTo(new Vector2d(56, 56), Math.PI / 2)
+
 
                                 .build()
                 );
